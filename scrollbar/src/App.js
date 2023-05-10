7c31677ba83a107ff766d2b20a0155abb52ab2b1
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-05-10 23:03:37 +0530

    Done

---
+++ scrollbar/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Components/Atoms/Card';
+
+function App() {
+  return (
+    <div className="App">
+    <Card/>
+    </div>
+  );
+}
+
+export default App;
